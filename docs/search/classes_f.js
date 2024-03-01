de84082feadb260094b175591c7bfcce91d41437
Marc Delomez <marc.delomez@free.fr>
2024-03-01 15:25:11 +0100

    updare doc

--- docs/search/classes_f.js
+++ docs/search/classes_f.js
@@ -1,5 +1,8 @@
 var searchData=
 [
+  ['wavefile',['WaveFile',['../class_wave_file.html',1,'']]],
+  ['waveplayer',['WavePlayer',['../class_wave_player.html',1,'']]],
+  ['waveplayerreader',['WavePlayerReader',['../class_wave_player_reader.html',1,'']]],
   ['window',['Window',['../class_window.html',1,'']]],
   ['wndattr',['WndAttr',['../class_wnd_attr.html',1,'']]],
   ['wndgraphic',['WndGraphic',['../class_wnd_graphic.html',1,'']]]
