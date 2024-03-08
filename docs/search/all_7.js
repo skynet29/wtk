a5d72c8bf8877bd5d1af9170bde7c45a9d932712
Marc Delomez <marc.delomez@free.fr>
2024-03-08 13:15:59 +0100

    update doc

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['horzdividerctrl',['HorzDividerCtrl',['../class_horz_divider_ctrl.html',1,'']]],
-  ['horzsizepanel',['HorzSizePanel',['../class_horz_size_panel.html',1,'']]]
+  ['horzdividerctrl',['HorzDividerCtrl',['../class_horz_divider_ctrl.html',1,'']]]
 ];
--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -5,6 +5,5 @@ var searchData=
   ['vector_3c_20treenode_20_2a_3e',['Vector&lt; TreeNode *&gt;',['../class_vector.html',1,'']]],
   ['vector_3c_20window_20_2a_3e',['Vector&lt; Window *&gt;',['../class_vector.html',1,'']]],
   ['vertdividerctrl',['VertDividerCtrl',['../class_vert_divider_ctrl.html',1,'']]],
-  ['vertsizepanel',['VertSizePanel',['../class_vert_size_panel.html',1,'']]],
   ['videostream',['VideoStream',['../class_video_stream.html',1,'']]]
 ];
